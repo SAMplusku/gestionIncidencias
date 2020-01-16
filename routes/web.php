2189c1a2b29d0fdd48a9d6e27eabe2e8c7447a0a
Arkaitz <97agalisteo@sanferminikastola.org>
2020-01-16 10:03:46 +0100

    Master, login y signup

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function (){
+    return view('login');
+})->name('login');
+Route::get('/signup', function (){
+    return view('signup');
+})->name('signup');
