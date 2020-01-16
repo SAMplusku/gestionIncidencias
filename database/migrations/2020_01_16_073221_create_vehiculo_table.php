221d5981b8a932129710bd76118ea16955f38654
Miguel <miguel.barros@ikasle.egibide.oeg>
2020-01-16 09:52:47 +0100

    base de datos

---
+++ database/migrations/2020_01_16_073221_create_vehiculo_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiculoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehiculo', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("matricula");
+            $table->string("tipo");
+            $table->string("modelo");
+            $table->integer("kilometraje");
+            $table->string("marca");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehiculo');
+    }
+}
