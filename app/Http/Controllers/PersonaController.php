33b1030a63a2ad600913bf0bfd9b75b20c0a553e
Miguel <miguel.barros@ikasle.egibide.oeg>
2020-01-17 13:10:25 +0100

    perfil de puta madre

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Coordinadore;
+use App\Incidencia;
 use App\Login;
 use App\Operadore;
+use App\Persona;
 use App\Tecnico;
 use Illuminate\Http\Request;
 
@@ -12,10 +14,16 @@ use Illuminate\Http\Request;
 class PersonaController extends Controller
 {
     public function show($id) {
-        $tecnico = Tecnico::all()->where('id',$id);
+        $tecnico = Tecnico::all()->where('id_persona',$id);
+        $tecnicoPersona = Persona::all()->where('id',$id);
+        $incidenciasOperador = Incidencia::all()->where('id_operador',$id)->count();
+        $incidenciasTecnico = Incidencia::all()->where('id_tecnico',$id)->count();
 
         return view("perfil", [
-            "persona" => $tecnico
+            "persona" => $tecnico[0],
+            "persona2" => $tecnicoPersona[0],
+            "tecnico" => $incidenciasTecnico,
+            "operador" => $incidenciasOperador
         ]);
     }
 }
