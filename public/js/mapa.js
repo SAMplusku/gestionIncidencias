264883a8881eee01a864c6191504f8e6a3d1fcce
sergiozulueta <sergio.zulueta@ikasle.egibide.org>
2020-01-23 12:58:12 +0100

    mapa funcionando

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -1,1 +1,9 @@
 
+function map(){
+    let map = L.map('map').setView([42.866924, -2.676800], 8);
+
+    L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=TALcQipMfxgGJSNPScri', {
+        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+
+    }).addTo(map);
+}
