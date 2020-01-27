80ee89317e1b8d97a74207f56e43c10ed12a0ba2
Arkaitz <97agalisteo@sanferminikastola.org>
2020-01-27 13:51:26 +0100

    27/01/2020

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,10 @@ function añadirTec(tipo) {
 
     if (tipo == 'tecnico'){
         $('#tecnicoDatos').append(
-            '<input type="text" name="localizacion" class="form-control" placeholder="Localización" required><br>'+
-            '<input type="text" name="especializacion" class="form-control" placeholder="Especialización" required><br>'+
+            '<select name="localizacion" class="form-control"><option disabled selected>Localización</option><option value="42.86692483027981, -2.676793844898662">Álava/Araba</option><option value="43.322196127201764, -1.975069515458618">Guipúzcoa/Guipuzkoa</option><option value="43.25920592943639, -2.938555670101835">Vizcaya/Bizkaia</option></select>'+
+            '<select name="especializacion" class="form-control"><option disabled selected>Especialización</option><option value="pinchazo">Pinchazo</option><option value="motor">Motor</option><option value="averia">Averia</option><option value="otros">Otros</option></select>'+
             '<select name="jornada" class="custom-select"><option disabled selected>Jornada</option><option value="mañana">Mañana</option><option value="tarde">Tarde</option><option value="noche">Noche</option></select><br><br>',
-            '<select name="comunidad" class="custom-select"><option disabled selected>Comunidad</option><option value="alava">Alava/Araba</option><option value="Vizcaya">Vizcaya/Bizkaia</option><option value="guipuzcoa">Guipúzcoa/Gipuzkoa</option></select><br>'
+            '<select name="comunidad" class="custom-select"><option disabled selected>Comunidad</option><option value="alava">Alava/Araba</option><option value="vizcaya">Vizcaya/Bizkaia</option><option value="guipuzcoa">Guipúzcoa/Gipuzkoa</option></select><br><br>'
         )
     }else{
         let borrar = document.getElementById("tecnicoDatos");
